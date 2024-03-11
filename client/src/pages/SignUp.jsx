53ea125a6737d9a37fff0a5131911b8a749ed433
Kowshik Valipireddy <139107793+kowshik3383@users.noreply.github.com>
2024-03-11 19:36:13 +0530

    Update SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -17,7 +17,7 @@ export default function SignUp() {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('https://mern-hotel-website-ig46.vercel.app/', {
+      const res = await fetch('https://mern-hotel-website-ig46.vercel.app/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
