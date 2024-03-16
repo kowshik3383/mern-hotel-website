160fa64f696b654dc5e4f6fe290dab466748a844
Kowshik Valipireddy <139107793+kowshik3383@users.noreply.github.com>
2024-03-17 00:11:59 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ mongoose.connect(uri)
 app.use(express.json());
 
 // Route for handling requests related to users
-app.use('/api/user', userRoute);
-app.use('/api/auth',authRoute);
+app.use('/server/user', userRoute);
+app.use('/server/auth',authRoute);
 
 // Error handler for invalid routes
 app.use((req, res, next) => {
